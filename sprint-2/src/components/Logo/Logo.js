4b4aaa92ae29336fa6838bfc61e68444aae227dd
Omar-Said <omar.a.said@outlook.com>
2020-11-15 22:09:04 -0500

    Final Commit

--- sprint-2/src/components/Logo/Logo.js
+++ sprint-2/src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/Logo/Logo-brainflix.svg";
 import "./Logo.scss";
 
-function Logo(props) {
+function Logo() {
   return (
     <div className="logo-container">
       <Link to="/">
