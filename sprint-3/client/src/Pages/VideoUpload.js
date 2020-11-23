06162201e2f0171ff0d1819c09e719386c13307a
Omar-Said <omar.a.said@outlook.com>
2020-11-22 20:02:04 -0500

    Added route from Cancel button to home

--- sprint-3/client/src/Pages/VideoUpload.js
+++ sprint-3/client/src/Pages/VideoUpload.js
@@ -2,6 +2,7 @@ import "./VideoUpload.scss";
 import template from "../assets/Images/Upload-video-preview.jpg";
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 class VideoUpload extends React.Component {
   state = {
@@ -71,7 +72,9 @@ class VideoUpload extends React.Component {
               </div>
               <div className="publish">
                 <button className="publish__button">PUBLISH</button>
-                <p className="publish__cancel">CANCEL</p>
+                <Link to="/" className="publish__cancel">
+                  CANCEL
+                </Link>
               </div>
             </form>
           </div>
@@ -82,114 +85,3 @@ class VideoUpload extends React.Component {
 }
 
 export default VideoUpload;
-
-// import "./VideoUpload.scss";
-// import template from "../assets/Images/Upload-video-preview.jpg";
-// import React from "react";
-
-// class VideoUpload extends React.Component {
-//   state = {
-//     title: "",
-//     description: "",
-//     image: template,
-//   };
-
-//   updateTitle = (event) => {
-//     this.setState({
-//       title: event.target.value,
-//     });
-//   };
-
-//   updateDescription = (event) => {
-//     this.setState({
-//       description: event.target.value,
-//     });
-//   };
-
-//   // Better way to incorpate all of the inputs into one function
-
-//   handleClick = (event) => {
-//     event.preventDefault();
-//     console.log(this.state.title, this.state.description, this.state.image);
-//     // Add an axios post method here - figure out how to push that through JSON
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <section className="upload-main">
-//           <div className="upload-main__title-wrapper">
-//             <h1 className="upload-main__title">Upload Video</h1>
-//             <form className="upload-form">
-//               <div className="upload-form__wrapper">
-//                 <div>
-//                   <p className="upload-form__label">VIDEO THUMBNAIL</p>
-//                   <input
-//                     className="upload-form__vid"
-//                     type="image"
-//                     name="thumbnail"
-//                     onChange={this.updateimage}
-//                     value={this.state.image}
-//                     src={template}
-//                     alt="video"
-//                   />
-//                 </div>
-//                 <div className="upload-form__input">
-//                   <label className="upload-form__label">TITLE YOUR VIDEO</label>
-//                   <input
-//                     className="upload-form__title"
-//                     type="text"
-//                     name="title"
-//                     onChange={this.updateTitle}
-//                     value={this.state.title}
-//                     placeholder="Add a title to your video"
-//                     required
-//                   />
-//                   <label className="upload-form__label">
-//                     ADD A VIDEO DESCRIPTION
-//                   </label>
-//                   <input
-//                     className="upload-form__desc"
-//                     type="text"
-//                     name="description"
-//                     onChange={this.updateDescription}
-//                     value={this.state.description}
-//                     placeholder="Add a description of your video"
-//                     required
-//                   />
-//                 </div>
-//               </div>
-//               <div className="publish">
-//                 <button className="publish__button" onClick={this.handleClick}>
-//                   PUBLISH
-//                 </button>
-//                 <p className="publish__cancel">CANCEL</p>
-//               </div>
-//             </form>
-//           </div>
-//         </section>
-//       </div>
-//     );
-//   }
-// }
-
-// export default VideoUpload;
-
-// addVideo = (e) => {
-//   e.preventDefault();
-//   const title = e.target.title.value;
-//   const description = e.target.description.value;
-
-//   const object = {
-//     title: title,
-//     description: description,
-//   };
-//   axios.post("http://localhost:8080/videos", object).then((response) => {
-//     console.log(response);
-//     this.setState({
-//       title: "",
-//       description: "",
-//     });
-//   });
-//   console.log(object);
-// };
--- sprint-3/server/routes/videoRoute.js
+++ sprint-3/server/routes/videoRoute.js
@@ -57,23 +57,3 @@ router.post("/videos", (req, res) => {
 });
 
 module.exports = router;
-
-// router.post("/videos", (req, res) => {
-//   const newSideVid = {
-//     id: uniqid(),
-//     title: req.body.title,
-//     description: req.body.description,
-//     image: "https://i.imgur.com/l2Xfgpl.jpg",
-//     channel: "Omar Said",
-//     views: 0,
-//     likes: 0,
-//     duration: "4:27",
-//     timestamp: Date.now(),
-//     comments: [],
-//   };
-
-//   const newVideo = readMainVideos();
-//   newVideo.push(newSideVid);
-//   fs.writeFileSync("./data/mainVideos.json", JSON.stringify(newVideo));
-//   res.status(201).json(newVideo);
-// });
